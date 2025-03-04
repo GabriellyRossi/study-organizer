6072d32300996423397b687cf6abf06d77747c92
Serena <gabyboareto@hotmail.com>
2025-03-04 01:12:50 -0300

    inject StudyMaterialMapper and use it in StudyMaterialController
    Created StudyMaterialMapper to convert StudyMaterialDTO to StudyMaterial entity.
    Updated StudyMaterialController to use the mapper.

---
+++ src/main/java/com/study_organizer/mapper/StudyMaterialMapper.java
@@ -0,0 +1,20 @@
+package com.study_organizer.mapper;
+
+import com.study_organizer.dto.StudyMaterialDTO;
+import com.study_organizer.model.StudyMaterial;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudyMaterialMapper {
+
+    public StudyMaterial toEntity(StudyMaterialDTO materialDTO) {
+        StudyMaterial material = new StudyMaterial();
+        material.setSubject(materialDTO.getSubject());
+        material.setCourseName(materialDTO.getCourseName());
+        material.setAuthor(materialDTO.getAuthor());
+        material.setDate(materialDTO.getDate());
+        material.setLink(materialDTO.getLink());
+        material.setAdditionalInfo(materialDTO.getAdditionalInfo());
+        return material;
+    }
+}
