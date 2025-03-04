06c8eb100ad6ffeda6f9d12a0b8292669b706196
Serena <gabyboareto@hotmail.com>
2025-03-04 18:05:28 -0300

    fix: add missing import for UsernameNotFoundException in GlobalExceptionHandler

--- src/main/java/com/study_organizer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/study_organizer/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.study_organizer.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
